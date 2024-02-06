6ea588b2c591087a52851be223cf4029d8843cb3
Junpei1121 <momooooonnn@gmail.com>
2024-02-06 11:35:06 +0000

    繰り返し処理

--- lesson4.rb
+++ lesson4.rb
@@ -1,6 +1,22 @@
-puts "計算をはじめます2つの値を入力してください"
-input_key = gets.to_i
-input_keys = gets.to_i
-puts "計算結果を出力します"
-puts "#{input_key}+#{input_keys}=#{input_key + input_keys}"
-puts "計算を終了します"
+puts "計算をはじめます"
+puts "何回繰り返しますか？"
+
+count = gets.to_i
+
+i = 1
+while count != 0 do
+  if i == (count+1)
+    puts "計算を終了します"
+    break
+  end
+  puts "#{i}回目の計算"
+  i += 1
+  puts "2つの値を入力してください"
+  a = gets.to_i
+  b = gets.to_i
+  puts "計算結果を出力します"
+  puts "#{a} + #{b} = #{a+b}"
+  puts "#{a} - #{b} = #{a-b}"
+  puts "#{a} * #{b} = #{a*b}"
+  puts "#{a} / #{b} = #{a/b}"
+end
