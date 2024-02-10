aa238a9697f117bd25fd2b8d2772e4f4982e970f
Junpei1121 <momooooonnn@gmail.com>
2024-02-10 11:26:07 +0000

    シンボル

--- lesson4.rb
+++ lesson4.rb
@@ -1,15 +1,2 @@
-class Car
-  def run(distance)
-    puts "車で#{distance}キロ走ります。"
-  end
-end
-
-class Truck < Car
-  def run(distance)
-    super
-      puts "大きな荷物を載せて走ります。" 
-    end
-end
-
-truck = Truck.new
-truck.run(5)
+webcamp = :プログラミング学習
+puts webcamp
