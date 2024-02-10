f504dd1e2694128d4c92fb89977cce9537e2fc62
Junpei1121 <momooooonnn@gmail.com>
2024-02-10 11:28:22 +0000

    シンボル例

--- lesson4.rb
+++ lesson4.rb
@@ -1,10 +1,2 @@
-dog1 = "犬"
-dog2 = "犬"
-
-dog3 = :犬
-dog4 = :犬
-
-puts dog1.object_id
-puts dog2.object_id
-puts dog3.object_id
-puts dog4.object_id
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:太郎]
